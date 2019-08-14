7d01ccd27ee441e0548a9118f83f74afe522a947
konyonyo <yuichiro.utsumi@gmail.com>
2019-08-14 17:19:20 +0900

    add gather load

--- ld1d.s
+++ ld1d.s
@@ -1,3 +1,12 @@
+// assemble & execute command
+// $ agcc -static -march=armv8-a+sve ld1d.s
+// $ qemu-aarch64 -cpu max,sve-max-vq=2 a.out
+// expected output
+// val=5.534783
+// val=5.534783
+// val=5.534783
+// val=5.534783
+// val=5.534783
   .text
   .align 2
   .p2align 3,,7
@@ -6,14 +15,56 @@
 main:
   stp x29, x30, [sp, -16]!
   adr x1, D0
-  adr x2, D1
-  adr x3, D2
   ptrue p0.d, all
   // LD1D(scalar plus immediate)
-  ld1d z0.d, p0/z, [x1, #1, MUL VL]
+  ld1d z0.d, p0/z, [x1, #1, mul vl]
   adrp x0, str
   add x0, x0, :lo12:str
   bl printf
+
+
+  adr x1, D0
+  adr x2, I0
+  ld1d z1.d, p0/z, [x2]
+  // LD1D(scalar plus vector)
+  // 64bit unscaled offset
+  ld1d z0.d, p0/z, [x1, z1.d]
+  adrp x0, str
+  add x0, x0, :lo12:str
+  bl printf
+
+  adr x1, D0
+  adr x2, I1
+  ld1d z1.d, p0/z, [x2]
+  // LD1D(scalar plus vector)
+  // 64bit scaled offset
+  ld1d z0.d, p0/z, [x1, z1.d, lsl #3]
+  adrp x0, str
+  add x0, x0, :lo12:str
+  bl printf
+
+  adr x1, D0
+  add x1, x1, #40
+  adr x2, I2
+  ld1d z1.d, p0/z, [x2]
+  // LD1D(scalar plus vector)
+  // 32bit unscaled offset
+  ld1d z0.d, p0/z, [x1, z1.d, sxtw]
+  adrp x0, str
+  add x0, x0, :lo12:str
+  bl printf
+
+  adr x1, D0
+  add x1, x1, #40
+  adr x2, I3
+  ld1d z1.d, p0/z, [x2]
+  // LD1D(scalar plus vector)
+  // 32bit scaled offset
+  ld1d z0.d, p0/z, [x1, z1.d, sxtw #3]
+  adrp x0, str
+  add x0, x0, :lo12:str
+  bl printf
+
   ldp x29, x30, [sp], 16
   ret
 D0:
@@ -93,6 +144,26 @@ D1:
   .double 2.0
 D2:
   .double 3.0
+I0:
+  .long 0x20, 0x0
+  .long 0x10, 0x0
+  .long 0x18, 0x0
+  .long 0x20, 0x0
+I1:
+  .long 0x4, 0x0
+  .long 0x2, 0x0
+  .long 0x3, 0x0
+  .long 0x4, 0x0
+I2:
+  .long -8, 0
+  .long -16, 0
+  .long -24, 0
+  .long -32, 0
+I3:
+  .long -1, 0
+  .long -2, 0
+  .long -3, 0
+  .long -4, 0
 str:
   .string "val=%lf\n"
 
